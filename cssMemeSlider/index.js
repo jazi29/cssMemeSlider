5eeb6d3369d7e1976bec57b8c45d7eab4a383f55
Zhazira Satbaeva <79010812+jazi29@users.noreply.github.com>
2023-09-15 18:43:10 +0600

    feat: add style.css and index.js (Fri, Sep 15, 6:43 PM)

---
+++ cssMemeSlider/index.js
@@ -0,0 +1,30 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide')
+const indicators = document.querySelectorAll('.indicator')
+const text = document.getElementById('text')
+const description = document.querySelectorAll('.meme-desc')
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('active_slide');
+            indicators[i].classList.add('active_indicator');
+            description[i].classList.add('active_text'); 
+        } else {
+            slide.classList.remove('active_slide');
+            indicators[i].classList.remove('active_indicator');
+            description[i].classList.remove('active_text'); 
+        }
+    });
+    text.textContent = slides[index].querySelector('p').textContent;
+}
+
+
+indicators.forEach((indicator, i) => {
+    indicator.addEventListener('click', () => {
+        currentSlide = i;
+        showSlide(currentSlide);
+    });
+});
+
+showSlide(currentSlide);
